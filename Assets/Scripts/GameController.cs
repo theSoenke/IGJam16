adc34560f84b00667a47b75b5f1df607f48303a0
kautschuckflasche <nilsw.agner@web.de>
2016-10-28 21:45:45 +0200

    GameController added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; } 
+
+    // Use this for initialization
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
