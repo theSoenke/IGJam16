07e071a66dd8647524a91d1bc6ac47e2a5adb7c0
Cyberlander <felix.froehlich1@gmx.net>
2016-10-28 22:05:11 +0200

    Element Folder

---
+++ Assets/Scripts/DesktopElementFolder.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DesktopElementFolder : MonoBehaviour,DesktopElementInterface {
+
+
+	string _elementName;
+	int _elementType;
+	double _desktopPositionX;
+	double _desktopPositionY;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public string getElementName()
+	{
+		return this._elementName;
+	}
+	public void setElementName(string name)
+	{
+		this._elementName = name;
+	}
+
+
+	public int getElementType()
+	{
+		return this._elementType;
+		
+	}
+
+	public void setElementType(int type)
+	{
+		this._elementType = type;
+	}
+
+	double getDesktopPositionX()
+	{
+		return this._desktopPositionX;
+	}
+	void setDesktopPositionX(double posX)
+	{
+		this._desktopPositionX = posX;
+	}
+
+	double getDesktopPositionY()
+	{
+		return this._desktopPositionY;
+	}
+
+	void setDesktopPositionY(double posY)
+	{
+		this._desktopPositionY = posY;
+	}
+
+	void performOnClickAction()
+	{
+		//do something
+	}
+
+}
