533a809304b6cc5b9739346868da90203c4190c0
Cyberlander <felix.froehlich1@gmx.net>
2016-10-29 01:51:19 +0200

    WorkOrder

---
+++ Assets/Scripts/DesktopElementWorkOrder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DesktopElementWorkOrder : MonoBehaviour {
+
+	public Image _assignedImage;
+	public Sprite _assignedSprite;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_assignedImage = GetComponent<Image> ();
+		_assignedImage.overrideSprite = _assignedSprite;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
