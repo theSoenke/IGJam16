5b940c85bc5f032e0029cd6459cbb89dfc4e9278
theSoenke <soenke.behrendt@outlook.com>
2016-10-28 21:26:37 +0200

    Add base files

---
+++ Assets/Scripts/Desktop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Desktop : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
