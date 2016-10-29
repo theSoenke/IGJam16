0ef39fd70b128e9a51efa7e6e021aa28bd13ae0e
theSoenke <theSoenke@users.noreply.github.com>
2016-10-29 17:22:16 +0200

    Prefetch images, image viewer close button

--- Assets/Scripts/ImageFetcher.cs
+++ Assets/Scripts/ImageFetcher.cs
@@ -1,15 +1,13 @@
 ﻿// Get the latest webcam shot from outside "Friday's" in Times Square
-using UnityEngine;
-using System;
+using SimpleJSON;
 using System.Collections;
 using System.Collections.Generic;
-using System.Text;
-using SimpleJSON;
-using System.IO;
+using UnityEngine;
 
-public class ImageFetcher {
+public class ImageFetcher
+{
 
-	/*private string _redditUrl = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+    /*private string _redditUrl = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
 	private string _clientId = "UqZAQ0ngVptioA";
 	private string _state = "make random string"; // TODO
 	private string _redirectUrl = "http%3A%2F%2F127.0.0.2";
@@ -27,45 +25,50 @@ public class ImageFetcher {
 		return url;
 	}*/
 
-	private ImageReceiver _imageReceiver;
+    private ImageReceiver _imageReceiver;
 
-	public ImageFetcher(ImageReceiver imageReceiver) {
-		_imageReceiver = imageReceiver;
-	}
+    public ImageFetcher(ImageReceiver imageReceiver)
+    {
+        _imageReceiver = imageReceiver;
+    }
 
-	public IEnumerator FetchImage(string title, string path)
-	{
-		WWW www = new WWW(path);
-		yield return www;
+    public IEnumerator FetchImage(string title, string path)
+    {
+        WWW www = new WWW(path);
+        yield return www;
 
-		Texture2D tex = new Texture2D(2, 2);
-		if (!tex.LoadImage (www.bytes)) {
-			Debug.Log ("Failed to download image " + path);
-		}
+        Texture2D tex = new Texture2D(2, 2);
+        if (!tex.LoadImage(www.bytes))
+        {
+            Debug.Log("Failed to download image " + path);
+        }
 
-		_imageReceiver.OnNewImage (title, tex);
-	}
+        _imageReceiver.OnNewImage(title, tex);
+    }
 
-	public IEnumerator GetGallery() {
-		Debug.Log ("Starf loading images");
+    public IEnumerator GetGallery()
+    {
+        Debug.Log("Starf loading images");
 
-		// string url = MakeAuthReuest ();
-		Dictionary<string, string> headers = new Dictionary<string, string>();
-		headers["Authorization"] = "Client-ID " + "24638144948bc87"; 
+        // string url = MakeAuthReuest ();
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        headers["Authorization"] = "Client-ID " + "24638144948bc87";
 
-		WWW www = new WWW("https://api.imgur.com/3/gallery/r/funny", null, headers);
-		yield return www;
+        WWW www = new WWW("https://api.imgur.com/3/gallery/r/adviceanimals", null, headers);
+        yield return www;
 
-		var json = JSON.Parse(www.text);
-		var data = json ["data"];
+        var json = JSON.Parse(www.text);
+        var data = json["data"];
 
-		for (int i = 0; i < data.Count; i++) {
-			var image = data [i];
-			if (!image ["animated"].AsBool && !image ["is_album"].AsBool) {
-				yield return FetchImage (image ["title"], image ["link"]);
-			}
-		}
+        for (int i = 0; i < data.Count; i++)
+        {
+            var image = data[i];
+            if (!image["animated"].AsBool && !image["is_album"].AsBool)
+            {
+                yield return FetchImage(image["title"], image["link"]);
+            }
+        }
 
-		Debug.Log ("Done loading images");
-	}
+        Debug.Log("Done loading images");
+    }
 }
