0ba4061a91223231283331dae1dd44434c6575c6
kautschuckflasche <nilsw.agner@web.de>
2016-10-29 02:59:48 +0200

    scorebard works

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -7,23 +7,25 @@ public class Scoreboard : MonoBehaviour
 {
     private float _score = 0;
 
-    private float _lastDisplayUpdate = Time.time;
+    private float _lastDisplayUpdate;
 
 
     public float scorePerSecond = 1;
+    public float scoreFactor = 1;
+    public float refreshTick = 1;
     public Text scoreDisplay;
 
     void Start()
     {
-
+        _lastDisplayUpdate = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        _score += scorePerSecond/Time.deltaTime;
-        if (Time.deltaTime - _lastDisplayUpdate > 1)
+        _score += scorePerSecond * Time.deltaTime * scoreFactor;
+        if (Time.time - _lastDisplayUpdate > refreshTick)
         {
             _lastDisplayUpdate = Time.time;
             scoreDisplay.text = Mathf.RoundToInt(_score).ToString();
