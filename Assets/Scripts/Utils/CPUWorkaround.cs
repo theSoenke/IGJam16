3261d2280ec0e34ba595f3e5f3659ab0473df422
theSoenke <theSoenke@users.noreply.github.com>
2016-10-29 20:30:17 +0200

    Fix standalone build

--- Assets/Scripts/Utils/CPUWorkaround.cs
+++ Assets/Scripts/Utils/CPUWorkaround.cs
@@ -1,15 +1,17 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
 public class CPUWorkaround : EditorWindow
 {
-	[MenuItem("Window/CPU Workaround")]
-	public static void ShowWindow()
-	{
-		EditorWindow.GetWindow<CPUWorkaround>("100% CPU Workaround", true, typeof(EditorWindow)).minSize = Vector2.zero;
-	}
-	void OnGUI()
-	{
-		this.Repaint ();
-	}
+    [MenuItem("Window/CPU Workaround")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<CPUWorkaround>("100% CPU Workaround", true, typeof(EditorWindow)).minSize = Vector2.zero;
+    }
+    void OnGUI()
+    {
+        this.Repaint();
+    }
 }
+#endif
