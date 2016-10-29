4e87fee71d42187cecb2848401b853b859ee177c
theSoenke <theSoenke@users.noreply.github.com>
2016-10-29 20:02:05 +0200

    Fix image slider button

--- Assets/Scripts/Desktop/DesktopController.cs
+++ Assets/Scripts/Desktop/DesktopController.cs
@@ -6,11 +6,11 @@ public class DesktopController : MonoBehaviour
     public GameObject itemPrefab;
     public GameObject trashPrefab;
     public int gridSize = 8;
-	public int padding = 40;
-	public int workOrderAreaWidth = 500;
-	public int workOrderAreaHeight = 600;
-	public int tileWidth;
-	public int tileHeight;
+    public int padding = 40;
+    public int workOrderAreaWidth = 500;
+    public int workOrderAreaHeight = 600;
+    public int tileWidth;
+    public int tileHeight;
 
     private int _spawnedItems;
     private IDesktopItem[,] _itemGrid;
@@ -18,8 +18,8 @@ public class DesktopController : MonoBehaviour
 
     void Start()
     {
-		tileWidth = (workOrderAreaWidth - (padding * 2)) / gridSize;
-		tileHeight = (workOrderAreaHeight - (padding * 2)) / gridSize;
+        tileWidth = (workOrderAreaWidth - (padding * 2)) / gridSize;
+        tileHeight = (workOrderAreaHeight - (padding * 2)) / gridSize;
         _itemGrid = new IDesktopItem[gridSize, gridSize];
         _itemGrid[0, 0] = new DesktopFolderTrash();
         var pos = new Vector3(0, 0, 0);
@@ -51,17 +51,17 @@ public class DesktopController : MonoBehaviour
             if (_itemGrid[randX, randY] == null)
             {
                 var pos = new Vector3(randX, randY, 0);
-				Debug.Log (pos);
-				pos.x *= tileWidth;
-				pos.y *= tileHeight;
-				Debug.Log (pos);
-				Debug.Log (padding);
-				pos += new Vector3 (padding, padding, 0);
-				Debug.Log ("after " + pos.ToString());
+                Debug.Log(pos);
+                pos.x *= tileWidth;
+                pos.y *= tileHeight;
+                Debug.Log(pos);
+                Debug.Log(padding);
+                pos += new Vector3(padding, padding, 0);
+                Debug.Log("after " + pos);
                 GameObject itemObject = (GameObject)Instantiate(itemPrefab);
                 itemObject.transform.SetParent(screen);
-				itemObject.transform.localPosition = pos;
-				_itemGrid[randX, randY] = itemObject.GetComponent<DesktopWorkItem>();
+                itemObject.transform.localPosition = pos;
+                _itemGrid[randX, randY] = itemObject.GetComponent<DesktopWorkItem>();
                 _spawnedItems++;
                 break;
             }
