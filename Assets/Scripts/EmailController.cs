9844437ace457eaa997cf0c333fcf34de332ef88
theSoenke <theSoenke@users.noreply.github.com>
2016-10-29 23:55:27 +0200

    Basic worker gui

--- Assets/Scripts/EmailController.cs
+++ Assets/Scripts/EmailController.cs
@@ -4,10 +4,12 @@ using UnityEngine.UI;
 public class EmailController : MonoBehaviour
 {
     public Text mailText;
+    public GameObject emailView;
 
 
     public void SetMailText(string content)
     {
+        emailView.SetActive(true);
         mailText.text = content;
     }
 }
