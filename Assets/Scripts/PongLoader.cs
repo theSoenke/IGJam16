58194aa66617c2f350420b283285f42fcf972351
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-10-29 19:20:35 +0200

    Add pong button

---
+++ Assets/Scripts/PongLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PongLoader : MonoBehaviour {
+
+	public void LoadPong() {
+		SceneManager.LoadScene ("Pong", LoadSceneMode.Additive);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
