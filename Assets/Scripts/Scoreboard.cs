e9f7d5b9b7fb6e8e1ae2278a71171022a7d4ed62
kautschuckflasche <nilsw.agner@web.de>
2016-10-29 02:45:08 +0200

    revert stuff

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions.Comparers;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    private float _score = 0;
+
+    private float _lastDisplayUpdate = Time.time;
+
+
+    public float scorePerSecond = 1;
+    public Text scoreDisplay;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _score += scorePerSecond/Time.deltaTime;
+        if (Time.deltaTime - _lastDisplayUpdate > 1)
+        {
+            _lastDisplayUpdate = Time.time;
+            scoreDisplay.text = Mathf.RoundToInt(_score).ToString();
+        }
+    }
+}
