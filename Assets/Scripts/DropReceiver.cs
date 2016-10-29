65d14b51eab1c210b26d4081901983a371ecf855
theSoenke <thesoenke@outlook.com>
2016-10-29 12:02:09 +0200

    Rename prefabs, folder hover effect

--- Assets/Scripts/DropReceiver.cs
+++ Assets/Scripts/DropReceiver.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DropReceiver : MonoBehaviour, IDropHandler
+public class DropReceiver : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public Color hoverColor;
+
+    private Color normalColor;
+    private Image image;
+
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        normalColor = image.color;
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         GameObject itemDragged = DragHandler.itemDragged;
         itemDragged.transform.SetParent(transform);
         Destroy(itemDragged);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.color = normalColor;
     }
 }
