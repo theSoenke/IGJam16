5c61fc4d44f1a67681a483df474a426a1046eb98
theSoenke <thesoenke@outlook.com>
2016-10-29 12:29:26 +0200

    Desktop Trash refactor

---
+++ Assets/Scripts/Desktop/DesktopFolderTrash.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DesktopFolderTrash : DesktopFolder
+{
+    public Image assignedImage;
+    public Sprite assignedSprite;
+
+    private enum ElementType { Folder = 1, Trash = 2, WorkOrder = 3 };
+
+
+    void Start()
+    {
+        assignedImage = GetComponent<Image>();
+        assignedImage.overrideSprite = assignedSprite;
+
+        elementType = (int)ElementType.Trash;
+    }
+
+    public string getElementName()
+    {
+        return elementName;
+    }
+    public void setElementName(string name)
+    {
+        elementName = name;
+    }
+
+
+    public int GetElementType()
+    {
+        return elementType;
+    }
+
+    public void SetElementType(int type)
+    {
+        elementType = type;
+    }
+}
