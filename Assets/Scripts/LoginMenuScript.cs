667b5a5c814aba790a92f00744150cc31980a506
Cyberlander <felix.froehlich1@gmx.net>
2016-10-30 02:30:48 +0200

    PlayerName wird persistent gespeichert

--- Assets/Scripts/LoginMenuScript.cs
+++ Assets/Scripts/LoginMenuScript.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class LoginMenuScript : MonoBehaviour {
 
+	public InputField _inputFieldUserName;
+	string _userName;
+
 	public void ChangeScene(string sceneName)
 	{
+		_userName = _inputFieldUserName.text.ToString();
+		PlayerPrefs.SetString ("SavedPlayerName", _userName);
+		Debug.Log (PlayerPrefs.GetString("SavedPlayerName"));
 		Application.LoadLevel (sceneName);
+	}
+
+	public void Shutdown()
+	{
+		Debug.Log ("Shutdown");
+		Application.Quit ();
 	}
 }
